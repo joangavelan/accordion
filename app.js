6574ea81e4eaffe3db982f95e7cc3c53e31d16c1
Johan Gavelan <johangavelan@gmail.com>
2021-01-17 11:25:31 -0500

    own solution

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-const accordionItems = document.getElementsByClassName("accordion__item");
+const headings = document.getElementsByClassName('accordion__heading');
 
-function accordion() {
-    for (let item of accordionItems) {
-        let accHeader = item.firstElementChild;
-        accHeader.addEventListener("click", function() {
-            let accDetails = this.nextElementSibling;
-            if (accDetails.style.maxHeight) {
-                accDetails.style.maxHeight = null;
-                this.lastElementChild.innerHTML = "+";
-            } else {
-                accDetails.style.maxHeight = accDetails.scrollHeight + "px";
-                this.lastElementChild.innerHTML = "-";
-            }
-        })
-    }
+for(let heading of headings) {
+    heading.addEventListener('click', function() {
+        let details = this.nextElementSibling;
+        let icon = this.lastElementChild;
+        
+        if(details.style.maxHeight) {
+            icon.innerHTML = '+';
+            details.style.maxHeight = null;
+        }
+        else {
+            details.style.maxHeight = details.scrollHeight + 'px';
+            icon.innerHTML = '-';
+        }
+    })
 }
-
-accordion();
